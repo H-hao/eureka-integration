991b177e111bf6a919a835b1d73e1b69e7991cc7
huanghao <huanghao@example.com>
2019-04-28 21:10:46 +0800

    统一为 yml； 为 HystrixDashboard 的数据来源项目 注入  HystrixMetricsStreamServlet；

--- eureka-consumer-feign/src/main/java/com/tutorial/springcloud/eurekaconsumerfeign/EurekaConsumerFeignApplication.java
+++ eureka-consumer-feign/src/main/java/com/tutorial/springcloud/eurekaconsumerfeign/EurekaConsumerFeignApplication.java
@@ -1,14 +1,27 @@
 package com.tutorial.springcloud.eurekaconsumerfeign;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
 public class EurekaConsumerFeignApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaConsumerFeignApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsumerFeignApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("hystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
--- hystrix-dashboard/src/main/java/com/tutorial/springcloud/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/tutorial/springcloud/hystrixdashboard/HystrixDashboardApplication.java
@@ -2,11 +2,9 @@ package com.tutorial.springcloud.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableHystrix
 @EnableHystrixDashboard
 public class HystrixDashboardApplication {
 
