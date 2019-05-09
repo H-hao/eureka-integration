39db000fdab5d9539a884b29f9a55536d97b3d19
huanghao <huanghao@example.com>
2019-05-09 12:32:44 +0800

    规范配置:service-url.defaultZone;

--- eureka-producer/src/main/java/com/tutorial/springcloud/eurekaproducer/EurekaProducerApplication.java
+++ eureka-producer/src/main/java/com/tutorial/springcloud/eurekaproducer/EurekaProducerApplication.java
@@ -2,13 +2,12 @@ package com.tutorial.springcloud.eurekaproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 // EnableEurekaClient 与 EnableDiscoveryClient 现在都不需要手动配置，因为其已根据引入的包来进行自动注册；
 public class EurekaProducerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaProducerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProducerApplication.class, args);
+    }
 }
