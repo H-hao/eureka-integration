8b9723ec7ee0ec2df3a02b9a4f674c679d0b577a
huanghao <huanghao@example.com>
2019-05-09 15:26:06 +0800

    集成 config 中心； 改造 eureka-consumer-feign 为 config client；

--- eureka-consumer-feign/src/main/java/com/tutorial/springcloud/eurekaconsumerfeign/HelloController.java
+++ eureka-consumer-feign/src/main/java/com/tutorial/springcloud/eurekaconsumerfeign/HelloController.java
@@ -1,6 +1,8 @@
 package com.tutorial.springcloud.eurekaconsumerfeign;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,12 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/hello")
+@RefreshScope
 public class HelloController {
     @Autowired
     HelloClient helloClient;
+    @Value("${config.hello}")
+    String hello;
 
     @GetMapping("/h")
     public String hello(@RequestParam("name") String name) {
-        return helloClient.hello(name + "! by feign");
+        return helloClient.hello(name + "! by feign" + " of " + hello);
     }
 }
