7ed8f6682fb9a72648c205adf5afea8fa1fdb318
huanghao <huanghao@example.com>
2019-06-03 20:59:01 +0800

    新增：spring cloud gateway 模块；

---
+++ api-gateway-gateway/src/main/java/apigatewaygateway/ApiGatewayGatewayApplication.java
@@ -0,0 +1,13 @@
+package apigatewaygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayGatewayApplication.class, args);
+    }
+
+}
