f745a95fbceaad3d898f65c6c42ea56053bd18b3
huanghao <huanghao@example.com>
2019-06-11 21:07:49 +0800

    新增：集成 路由熔断；

---
+++ api-gateway-gateway/src/main/java/apigatewaygateway/web/FallbackController.java
@@ -0,0 +1,19 @@
+package apigatewaygateway.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by huanghao on 2019/6/11 0011.
+ *
+ * @author huanghao
+ */
+@RestController
+public class FallbackController {
+
+    @GetMapping("/fallback")
+    public String fallback() {
+        return "Hello World!\nfrom gateway";
+    }
+
+}
